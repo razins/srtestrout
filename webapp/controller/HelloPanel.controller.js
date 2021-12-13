b16e5d9a241dbd144f2f7e58d99c855eb9a9f87e
razins <serk@msx.ru>
2021-12-13 11:59:16 +0300

    commit1

---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+
+		onShowHello : function () {
+         alert('Hello')
+		},
+		onOpenDialog : function () {
+
+			// create dialog lazily
+			if (!this.pDialog) {
+				this.pDialog = this.loadFragment({
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				});
+			} 
+			this.pDialog.then(function(oDialog) {
+				oDialog.open();
+			});
+		},
+		onCloseDialog : function () {
+			// note: We don't need to chain to the pDialog promise, since this event-handler
+			// is only called from within the loaded dialog itself.
+			this.byId("helloDialog").close();
+		}		
+
+
+	});
+});
